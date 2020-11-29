25876bd5c31658b7ea4acd08b9aea7405bcba245
Nkeiruka <58699506+Kxiru@users.noreply.github.com>
2020-11-29 01:48:56 +0000

    Sync Merge ImplementingStacks branch

--- Calculator-CS2800-1/src/main/java/uk/ac/rhul/cs2800/OpStack.java
+++ Calculator-CS2800-1/src/main/java/uk/ac/rhul/cs2800/OpStack.java
@@ -2,25 +2,25 @@ package uk.ac.rhul.cs2800;
 
 import java.util.EmptyStackException;
 
-public class NumStack {
-  private Stack numStack;
+public class OpStack {
+  private Stack opStack;
 
   /**
-   * A constructor for the composed class, NumStack. NumStack is a facade that is built using
-   * objects from another class (Stack).
+   * A constructor for the composed class, OpStach. Opstack is a facade that is built using objects
+   * from another class (Stack). It addresses operators in the calculator.
    */
-  public NumStack() {
-    numStack = new Stack();
+  public OpStack() {
+    opStack = new Stack();
   }
 
   /**
-   * The Push() method adds entries to the stack. It uses the method from Stack to push the Float
+   * The Push() method adds entries to the stack. It uses the method from Stack to push the Symbol
    * Type Entry to the stack.
    * 
-   * @param number A float type input that is passed into the stack.
+   * @param i A Symbol (enum) type input that is passed into the stack.
    */
-  public void push(Float number) {
-    numStack.push(new Entry(number));
+  public void push(Symbol i) {
+    opStack.push(new Entry(i));
   }
 
   /**
@@ -28,15 +28,15 @@ public class NumStack {
    * uses LIFO (Last in, First Out) implementation. An exception is thrown if the size of the list
    * is zero.
    * 
-   * @return Returns the value of the item at the top of the stack.
+   * @return Returns the Symbol of the item at the top of the stack.
    * @throws BadTypeException If the type is incorrect / invalid.
    */
-  public float pop() throws BadTypeException {
+  public Symbol pop() throws BadTypeException {
     if (this.isEmpty()) {
       throw new EmptyStackException();
     } else {
-      Entry entry = numStack.pop();
-      return entry.getValue();
+      Entry entry = opStack.pop();
+      return entry.getSymbol();
     }
   }
 
@@ -46,7 +46,7 @@ public class NumStack {
    * @return Returns false when the stack is not empty.
    */
   public boolean isEmpty() {
-    if (numStack.size() == 0) {
+    if (opStack.size() == 0) {
       return true;
     }
     return false;
